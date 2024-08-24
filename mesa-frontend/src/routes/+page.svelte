0ed1dded5514c06ef43b473d833d374222207676
victobonetti <victorbonettixd@gmail.com>
2024-08-24 16:51:32 -0300

    first commit

---
+++ mesa-frontend/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<main class="bg-neutral-50 text-neutral-900">
+	<h1 class="text-2xl font-bold">NaMesa</h1>
+</main>
+  
+<style lang="postcss">
+	:global(html) {
+	  background-color: theme(colors.gray.100);
+	}
+</style>
