17b351e688f1d017d0f54de5f826712322c92d42
victobonetti <victorbonettixd@gmail.com>
2024-09-11 00:23:50 -0300

    wip

--- mesa-frontend/src/routes/+page.svelte
+++ mesa-frontend/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import waiter from '$lib/assets/waiter.jpg';
 	import logo from '$lib/assets/logo.png';
+	import { Button } from "$lib/components/ui/button";
 </script>
 
 <div
@@ -8,10 +9,9 @@
 >
 	<div class="w-1/2 p-8 flex flex-col items-center">
 		<img class="w-64" src={logo} alt="logo">
-		<a
+		<Button class="mt-2"
 			href="/login"
-			class="text-neutral-50 mt-8 bg-neutral-800 w-32 py-2 px-6 rounded-full text-center"
-			>Iniciar</a
+			>Iniciar</Button
 		>
 	</div>
 	<div class="w-1/2 h-full">
--- mesa-frontend/src/routes/login/+page.svelte
+++ mesa-frontend/src/routes/login/+page.svelte
@@ -5,6 +5,10 @@
 	import logo from "$lib/assets/logo.png";
 	import { getContext } from "svelte";
 	import { handleResponse } from "../../services/handleResponse";
+	import { Button } from "$lib/components/ui/button";
+	import { Input } from "$lib/components/ui/input/index.js";
+	import { Label } from "$lib/components/ui/label/index.js";
+	import Reload from "svelte-radix/Reload.svelte";
 
 	const { throwError } = getContext("notify");
 	let email = "";
@@ -29,54 +33,64 @@
 	};
 </script>
 
-<div class=" h-96 w-96 border rounded shadow-lg bg-neutral-100 pt-8 flex flex-col items-center">
+<div
+	class=" h-96 w-96 border rounded shadow-lg bg-neutral-100 pt-8 flex flex-col items-center"
+>
 	<img class="w-64 mb-4" src={logo} alt="logo" />
 	<div class="h-56 flex flex-col items-center justify-center p-8">
 		{#if !loading}
-			<div class="w-72 mb-2">
+			<div class="w-72 mb-6">
 				<div class="relative w-full min-w-[200px] h-10">
-					<input
+					<Label for="email">Email</Label>
+					<Input
+						class="bg-neutral-50"
 						id="email"
 						bind:value={email}
 						type="text"
-						class="peer w-full h-full bg-neutral-50 text-neutral-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-neutral-300 focus:border-gray-900"
-						placeholder=" "
-					/><label
-						for="email"
-						class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate leading-tight peer-focus:leading-tight peer-disabled:text-transparent transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-neutral-500 peer-focus:text-neutral-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900"
-						>Email
-					</label>
+					/>
 				</div>
 			</div>
-			<div class="w-72 mb-4">
+			<div class="w-72 mb-12">
 				<div class="relative w-full min-w-[200px] h-10">
-					<input
+					<Label for="pass">Senha</Label>
+					<Input
+						class="bg-neutral-50"
 						id="pass"
 						bind:value={pass}
 						type="password"
-						class="peer w-full h-full bg-neutral-50 text-neutral-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-neutral-300 focus:border-gray-900"
-						placeholder=" "
-					/><label
-						for="pass"
-						class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate leading-tight peer-focus:leading-tight peer-disabled:text-transparent transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-neutral-500 peer-focus:text-neutral-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900"
-						>Senha
-					</label>
+					/>
 				</div>
 			</div>
 
-			<button
-				on:click={loginTrigger}
-				autofocus
-				class="text-center text-neutral-50 bg-neutral-800 w-32 py-2 px-6 rounded-full mb-4 cursor-pointer"
-				>Login</button
-			>
-			<a
-				href="/"
-				class="text-sm hover:underline text-neutral-500 cursor-pointer"
-				>Tela inicial</a
+			<Button on:click={loginTrigger} class="mb-4">Login</Button>
+			<Button variant="link" class="text-xs" href="/">Tela inicial</Button
 			>
 		{:else}
-			<h3 class=" select-none animate-pulse">Carregando...</h3>
+			<div class="w-72 mb-6">
+				<div class="relative w-full min-w-[200px] h-10">
+					<Label for="email">Email</Label>
+					<Input
+						disabled
+						class="bg-neutral-50"
+						id="email"
+						type="text"
+					/>
+				</div>
+			</div>
+			<div class="w-72 mb-12">
+				<div class="relative w-full min-w-[200px] h-10">
+					<Label for="pass">Senha</Label>
+					<Input
+						disabled
+						class="bg-neutral-50"
+						id="pass"
+						type="password"
+					/>
+				</div>
+			</div>
+
+			<Button class="mb-4" disabled>Por favor, aguarde</Button>
+			<Button variant="link" class="text-xs" disabled>Tela inicial</Button>
 		{/if}
 	</div>
 </div>
